cd07013f5f342ccad3a6ec7f128043685bf59ee7
Teena George <tegeorge@ap.equinix.com>
2018-01-19 00:08:37 +0800

    committing the completed methods for week3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix computations:
+## Creates and stores the matrix and inverse
 
-## Write a short comment describing this function
+## Creates a matrix object (y), 
+## returns the matrix value,
+## stores the inverse of the matrix,
+## and returns a list of all the above functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  y <- NULL
+  invY <- NULL
+  
+  get <- function(){
+    y <<- x
+    y
+  }
+  
+  inverse <- function(){
+    invY <- solve(x)
+    invY
+  }
+  list(get = get, inverse = inverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function calculates the inverse of the function.
+## The calculated value is stored in and fetched from
+##the cache for the same input matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverse()
+        matr <- x$get()
+        if(!is.null(inverse)){
+          message("Getting the cached data")
+          return(inverse)
+        }else{
+          inverse <- solve(matr, ...)
+          x$setInverse(inverse)
+          inverse
+        }
 }
